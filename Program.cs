f12bf9eb648e051fee47750b54eb6945f59b7c6f
y.abibok <yuraabibok@gmail.com>
2017-01-28 15:55:48 +0300

    Start Project

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Beep(1000, 1000);
+        }
+    }
+}
